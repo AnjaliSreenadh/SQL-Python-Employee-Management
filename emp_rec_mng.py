bd8a17b9aead97d900d9e068238b1c82e7a135f3
Anjali K S <160105968+AnjaliSreenadh@users.noreply.github.com>
2024-03-05 13:29:32 +0530

    Add files via upload

---
+++ emp_rec_mng.py
@@ -0,0 +1,95 @@
+import mysql.connector
+m=mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="101285",
+    database="assignment"
+)
+#c=m.cursor()
+#c.execute("create table emplrec(name varchar(50) primary key not null,age int,salary int)")#
+#m.commit()
+#c.close()
+def list_emp():
+    m = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="101285",
+        database="assignment"
+    )
+    c=m.cursor()
+    c.execute("Select * from emplrec")
+    rows=c.fetchall()
+    print("Name     Age     Salary")
+    for rows in rows:
+        for col in rows:
+            print(col,end='      ')
+        print()
+    c.close()
+def add_emp(name,age,sal):
+    m = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="101285",
+        database="assignment"
+    )
+    c=m.cursor()
+    s="insert into emplrec(name,age,salary)values(%s,%s,%s)"
+    v=[(nm,ag,sal)]
+    c.executemany(s, v)
+    m.commit()
+    m.close()
+def edit_emp(n,n_n,n_a,n_s):
+    m = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="101285",
+        database="assignment"
+    )
+    c=m.cursor()
+    s=("update emplrec set name=%s,age=%s,salary=%s where name=%s")
+    val=[(n_n,n_a,n_s,n)]
+    c.executemany(s,val)
+    m.commit()
+    m.close()
+def del_emp(n):
+    m = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="101285",
+        database="assignment"
+    )
+    c=m.cursor()
+    s=("delete from emplrec where name=%s")
+    val=[(n,)]
+    c.executemany(s,val)
+    m.commit()
+    m.close()
+ch=1
+while(ch<5):
+    print("\n1.List\n2.Add\n3.Edit\n4.Delete\n5.Exit\n")
+    ch=int(input("Please select your input: "))
+    if ch==1:
+        print("\nThe list of Employees")
+        list_emp()
+    elif ch==2:
+        nm=input("Enter the name: ")
+        ag=int(input("Enter the age: "))
+        sal=int(input("Enter the salary: "))
+        add_emp(nm,ag,sal)
+        print("\nEmployee Details Added Successfully..!!")
+    elif ch==3:
+        n=input("Enter the name of the Employee to be edited: ")
+        n_n=input("Enter the new name: ")
+        n_a=int(input("Enter the new age: "))
+        n_s=int(input("Enter the new salary: "))
+        edit_emp(n,n_n,n_a,n_s)
+        print("\nEmployee Details Edited Successfully..!!")
+
+    elif ch==4:
+        n = input("Enter the name of the employee to be deleted: ")
+        del_emp(n)
+        print("\nEmployee Details Deleted Successfully..!!")
+    else:
+        if ch==5:
+            continue
+        print("\nInvalid")
